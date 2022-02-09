a87520e014207c83ef79c7e5ee22d65cf07c58b6
msalves008 <matheusdesousaalves2018@gmail.com>
2022-02-08 23:15:04 -0300

    feat: create hook from get list all books

---
+++ src/hooks/useGetAllBooks.ts
@@ -0,0 +1,11 @@
+import useSWR from 'swr';
+import {api} from '../service/apiClient';
+
+export function getAllBooks<Data = any>(url: string) {
+  const {data, error} = useSWR<Data>(url, async url=>{
+    const response = await api.get(url);
+    const data = await response.data;
+    return data;
+  })
+  return {data, error};
+}
